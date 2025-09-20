a819d7b31bf7d6256590f0daf050a3d5a7232756
Lucas Debeve <81489208+LucasDebeve@users.noreply.github.com>
2025-09-20 12:12:28 +0200

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,10 +12,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://lucasdebeve.github.io/',
+  url: 'https://cours.lucasdebeve.eu/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/uranus-doc/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
